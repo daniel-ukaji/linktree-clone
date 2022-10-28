e52c253291ee77340afa98b474b3583500c221ca
Daniel Ukaji <dannyukaji@gmail.com>
2022-10-28 17:28:53 +0100

    New Commit.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,9 +7,9 @@ import I4G from '../images/I4G.png';
 function Footer() {
   return (
     <div className='flex flex-col items-center  space-y-4 mt-5 border-t pt-5 xl:flex xl:flex-row xl:justify-between xl:ml-80 xl:mr-80 xl:pt-5 xl:items-center xl:border-t xl:mt-20'>
-        <img src={Zuri} className='' />
+        <img src={Zuri} alt="footer logo" className='' />
         <h5 className='text-[#667085] '>HNG Internship 9 Frontend Task</h5>
-        <img src={I4G} className='' />
+        <img src={I4G} alt="footer logo" className='' />
     </div>
   )
 }
