e65b3928eb068002a04939303ea28af452a0c414
Daniel Ukaji <dannyukaji@gmail.com>
2022-10-29 12:12:12 +0100

    Final Commit.

--- src/App.js
+++ src/App.js
@@ -3,37 +3,46 @@ import profile_pic from '../src/images/profile__img.png';
 import slack from "../src/images/slack.png";
 import Icon from "../src/images/Icon.png";
 import Footer from './components/Footer';
+import shareIcon from "../src/images/Share-icon.png";
+import Content from "../src/images/Content.png";
 
 function App() {
   return (
     <div className="mt-5">
       <div className='flex justify-center mb-10 flex-col items-center space-y-4'>
-        <img src={profile_pic} alt="profile Pic" />
-        <h1 className='font-bold'>Annette Black</h1>
+        <div className="hidden md:block">
+          <img src={shareIcon} alt="share Icon" className='pl-96' />
+        </div>
+        <div className='md:hidden'>
+          <img src={Content} alt="share Content" className="ml-96 rounded-full border" />
+        </div>
+        
+          <img src={profile_pic} id="profile__img" alt="profile Pic" />
+        
+        <h1 className='font-bold'>Odogwu Senpai</h1>
       </div>
 
       <div className='flex justify-center flex-col items-center space-y-7'>
-        <div className='bg-[#EAECF0] w-3/5 flex justify-center pt-3 pb-3 items-center'>
-          <h5 className='font-semibold'>Twitter Link</h5>
-        </div>
-        <div className='bg-[#EAECF0] w-3/5 flex justify-center pt-3 pb-3 items-center'>
-          <h5 className='font-semibold'>Zuri Team</h5>
-        </div>
-        <div className='bg-[#EAECF0] w-3/5 flex justify-center pt-3 pb-3 items-center'>
-          <h5 className='font-semibold'>Zuri Books</h5>
-        </div>
-        <div className='bg-[#EAECF0] w-3/5 flex justify-center pt-3 pb-3 items-center'>
-          <h5 className='font-semibold'>Python Books</h5>
-        </div>
-        <div className='bg-[#EAECF0] w-3/5 flex justify-center pt-3 pb-3 items-center'>
-          <h5 className='font-semibold'>Background Check for Coders</h5>
-        </div>
-        <div className='bg-[#EAECF0] w-3/5 flex justify-center pt-3 pb-3 items-center'>
-          <h5 className='font-semibold'>Design Books</h5>
-        </div>
+        
+          <a href='https://twitter.com/ukaji_tech' id='twitter' className='font-semibold bg-[#EAECF0] w-3/5 text-center pt-3 pb-3 hover:bg-[#D0D5DD] rounded-lg' rel="noreferrer" target="_blank" >Twitter Link</a>
+        
+          <a href='https://training.zuri.team/' title='This is where you find books about design and code' id='btn__zuri' className='font-semibold bg-[#EAECF0] w-3/5 text-center pt-3 pb-3 hover:bg-[#D0D5DD] rounded-lg' rel="noreferrer" target="_blank">Zuri Team</a>
+        
+          <a href='https://books.zuri.team/python-for-beginners?ref_id=U048ZQ7508Z' title='Best book for beginners in Python' id='books' className='font-semibold bg-[#EAECF0] w-3/5 text-center pt-3 pb-3 hover:bg-[#D0D5DD] rounded-lg' rel="noreferrer" target="_blank">Zuri Books</a>
+        
+          <a href='https://books.zuri.team' title='We do the background check on coders to present you with the best candidates to build your product' id='book__python' className='font-semibold bg-[#EAECF0] w-3/5 text-center pt-3 pb-3 hover:bg-[#D0D5DD] rounded-lg' rel="noreferrer" target="_blank">Python Books</a>
+
+
+          <a href='https://background.zuri.team' title='Buy our free design book offered by Zuri' id='pitch' className='font-semibold bg-[#EAECF0] w-3/5 text-center pt-3 pb-3 hover:bg-[#D0D5DD] rounded-lg' rel="noreferrer" target="_blank">Background Check for Coders</a>
+       
+        
+          <a href='https://books.zuri.team/design-rules' id='book__design' className='font-semibold bg-[#EAECF0] w-3/5 text-center pt-3 pb-3 hover:bg-[#D0D5DD] rounded-lg' rel="noreferrer" target="_blank">Design Books</a>
+        
+        
       </div>
       <div className='flex justify-center mt-6 space-x-6'>
-        <img src={slack} alt="Slack" />
+        <img src={slack} alt="Slack" id="slack" />
+        <p className='hidden'>Slack ID: Odogwu_Senpai</p>
         <img src={Icon} alt="Icon" />
       </div>
       <Footer />
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ import I4G from '../images/I4G.png';
 
 function Footer() {
   return (
-    <div className='flex flex-col items-center  space-y-4 mt-5 border-t pt-5 xl:flex xl:flex-row xl:justify-between xl:ml-80 xl:mr-80 xl:pt-5 xl:items-center xl:border-t xl:mt-20'>
+    <div className='space-y-4 border-t pt-10 ml-10 mt-10 mb-10 mr-10 lg:flex lg:flex-row lg:justify-between lg:ml-64 lg:mr-64 lg:pt-5 lg:items-center lg:border-t lg:mt-20 lg:pb-7'>
         <img src={Zuri} alt="footer logo" className='' />
         <h5 className='text-[#667085] '>HNG Internship 9 Frontend Task</h5>
         <img src={I4G} alt="footer logo" className='' />
